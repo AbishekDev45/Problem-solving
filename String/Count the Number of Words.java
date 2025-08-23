8df544e9403d605ae0b506d4944cfbecb2e6fc8e
Abishekmani V <abishekmani264@gmail.com>
2025-08-23 22:42:45 +0530

    Create Count the Number of Words.java

---
+++ String/Count the Number of Words.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String s1 = s.nextLine();
+        String arr[] = s1.trim().split("\\s+");
+        int n=arr.length;
+        System.out.print(n);
+    }
+}
