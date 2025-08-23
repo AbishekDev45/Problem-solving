35fd7be53b98aaed1540317f530c70170f20044a
Abishekmani V <abishekmani264@gmail.com>
2025-08-23 22:47:41 +0530

    Create  Frequency of Characters in a String.java

---
+++ String/Frequency of Characters in a String.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String s1 = s.nextLine();
+        int n = s1.length();
+        int[] du =new int[256];       
+        for(int i=0;i<n;i++){
+              du[s1.charAt(i)]++;  
+        }
+        for(int i=0;i<n;i++){
+            if(du[s1.charAt(i)]!=0){
+            System.out.println(s1.charAt(i)+": "+ du[s1.charAt(i)]);
+             du[s1.charAt(i)] = 0;
+            }
+        }
+    }
+}
