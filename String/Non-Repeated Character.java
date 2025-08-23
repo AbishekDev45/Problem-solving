194623bf62d80571e78e820d69b2e49380c472bf
Abishekmani V <abishekmani264@gmail.com>
2025-08-23 22:45:58 +0530

    Create Non-Repeated Character.java

---
+++ String/Non-Repeated Character.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        int n = s.length();
+        int [] freq = new int[256];
+        for(int i=0;i<n;i++){
+           freq[s.charAt(i)]++;
+        }
+        for(int i=0;i<n;i++){
+            if(freq[s.charAt(i)] == 1) return i;
+        }
+        return -1;
+    }
+}
